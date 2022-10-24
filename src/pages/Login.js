2a6fc47c2ba220b970e020322517f595b6bc0d1f
Matheus França <matfbs1@gmail.com>
2022-10-24 16:38:32 -0300

    Requisito 03

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  // Só pro lint parar de reclamar.
+  console.log(setButtonDisabled);
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    switch (name) {
+    case 'emailInput':
+      setUserEmail(value);
+      break;
+    case 'passwordInput':
+      setUserPassword(value);
+      break;
+    default:
+      return true;
+    }
+  };
+
   return (
     <div>
       <input
@@ -8,16 +29,21 @@ function Login() {
         data-testid="email-input"
         name="emailInput"
         placeholder="Email"
+        value={ userEmail }
+        onChange={ handleChange }
       />
       <input
         type="password"
         data-testid="password-input"
         name="passwordInput"
         placeholder="Password"
+        value={ userPassword }
+        onChange={ handleChange }
       />
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ buttonDisabled }
       >
         Enter
       </button>
