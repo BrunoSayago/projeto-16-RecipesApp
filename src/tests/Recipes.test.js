cc4efc528185972f6b2f60f417d31f5322f4aca7
Peterson <petersonpaganelli@outlook.com>
2022-10-26 10:53:41 -0300

    Testes req 19

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import App from '../App';
 import Provider from '../context/Provider';
 import { renderWithRouter } from './helpers/renderWith';
-import foodData from './helpers/mockData';
 import drinks from '../../cypress/mocks/drinks';
-// import { ToggleButton } from 'react-bootstrap
+import meals from '../../cypress/mocks/meals';
 
 describe('Testes do componente Recipes', () => {
   test('Testa se a API correta é chamada na página meals', async () => {
@@ -17,7 +16,7 @@ describe('Testes do componente Recipes', () => {
       </Provider>,
     );
     jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue(foodData),
+      json: jest.fn().mockResolvedValue(meals),
     });
     act(() => {
       history.push('/meals');
@@ -27,6 +26,7 @@ describe('Testes do componente Recipes', () => {
     const corba = await screen.findByText('Corba');
     expect(corba).toBeInTheDocument();
     userEvent.click(corba);
+    await waitFor(() => expect(history.location.pathname).toBe('/meals/52977'));
   });
 
   test('Testa se a API correta é chamada na página drinks', async () => {
@@ -46,5 +46,50 @@ describe('Testes do componente Recipes', () => {
     const drink = await screen.findByText('GG');
     expect(drink).toBeInTheDocument();
     userEvent.click(drink);
+    await waitFor(() => expect(history.location.pathname).toBe('/drinks/15997'));
+  });
+
+  test('Testa os filtros da página meals', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(meals),
+    });
+    act(() => {
+      history.push('/meals');
+    });
+    const text = await screen.findByTestId('Pasta-category-filter');
+    expect(text).toBeInTheDocument();
+    userEvent.click(text);
+    const kumpir = await screen.findByText('Kumpir');
+    expect(kumpir).toBeInTheDocument();
+    const allBtn = await screen.findByTestId('All-category-filter');
+    userEvent.click(allBtn);
+    const corba = await screen.findByText('Corba');
+    expect(corba).toBeInTheDocument();
+  });
+
+  test('Testa os filtros da página drinks', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(drinks),
+    });
+    act(() => {
+      history.push('/drinks');
+    });
+    const text = await screen.findByTestId('Cocktail-category-filter');
+    expect(text).toBeInTheDocument();
+    userEvent.click(text);
+    const a1 = await screen.findByText('A1');
+    expect(a1).toBeInTheDocument();
+    userEvent.click(a1);
+    await waitFor(() => expect(history.location.pathname).toBe('/drinks/17222'));
   });
 });
