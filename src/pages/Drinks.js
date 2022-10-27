63b17368ff5af0c0ca8df0e365d3fd49828874d2
Stênio Santos <saugustomsantos@gmail.com>
2022-10-27 11:41:20 -0300

    Adiciona requisitos 16 ao 18 sem testes

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/Footer';
 // import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Recipes from '../components/Recipes';
@@ -9,6 +10,7 @@ function Drinks() {
       <Header />
       <h1>Drinks</h1>
       <Recipes />
+      <Footer />
     </div>
   );
 }
