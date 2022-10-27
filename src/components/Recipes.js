459f3b8c0cd988a1475898e4e2d59ad59083d344
Peterson <petersonpaganelli@outlook.com>
2022-10-27 09:53:50 -0300

    Conclusão dos testes

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
+import Context from '../context/Context';
 
 function Recipes() {
   const [listaReceitas, setListaReceitas] = useState([]);
   const [listaCategorias, setListaCategorias] = useState([]);
-  const [categoriaEscolhida, setCategoriaEscolhida] = useState('');
-  const [categoriaFiltrada, setCategoriaFiltrada] = useState('');
+  const { categoriaEscolhida,
+    setCategoriaEscolhida,
+    setCategoriaFiltrada,
+    categoriaFiltrada } = useContext(Context);
 
   const END_POINT_COMIDA = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
   const END_POINT_BEBIDA = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-
   const END_POINT_CAT_COMIDA = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
   const END_POINT_CAT_BEBIDA = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 
@@ -41,24 +43,6 @@ function Recipes() {
     };
     fetchCategorias();
   }, [pathname]);
-
-  useEffect(() => {
-    if (categoriaEscolhida) {
-      const fetchFiltered = async () => {
-        const END_POINT = pathname === '/meals'
-          ? `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoriaEscolhida}` : `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoriaEscolhida}`;
-        const data = await fetch(END_POINT);
-        const results = await data.json();
-        const { meals, drinks } = results;
-        const tipo = meals || drinks;
-        const MAX_POSITION = 12;
-        setCategoriaFiltrada(tipo.slice(0, MAX_POSITION));
-      };
-      fetchFiltered();
-    } else {
-      setCategoriaFiltrada('');
-    }
-  }, [categoriaEscolhida, pathname]);
 
   const handleFilterCategory = (event) => {
     if (categoriaEscolhida === event.target.innerText) {
