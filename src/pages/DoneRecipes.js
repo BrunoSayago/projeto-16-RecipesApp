bdbdc7e2f5ef493828fdaddd2f4139d673c3cf65
Peterson <petersonpaganelli@outlook.com>
2022-11-09 22:40:51 -0300

    Req 44 part 1

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -3,6 +3,8 @@ import Header from '../components/Header';
 // import { useHistory } from 'react-router-dom';
 
 function DoneRecipes() {
+  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
+
   return (
     <div>
       <Header />
@@ -11,6 +13,32 @@ function DoneRecipes() {
         <button type="button" data-testid="filter-by-food-btn">Food</button>
         <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
       </div>
+      {doneRecipes && doneRecipes.map((item, index) => (
+        <div key={ index }>
+          <img
+            width="100px"
+            data-testid={ `${index}-horizontal-image` }
+            src={ item.image }
+            alt="item"
+          />
+          {item.type === 'meal'
+            ? (
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${item.nationality} - ${item.category}`}
+
+              </p>
+            ) : (
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${item.alcoholicOrNot}`}
+
+              </p>
+            )}
+        </div>
+      ))}
     </div>
   );
 }
