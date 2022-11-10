fafdb104b86fcce097a510cbbb5928adf2e18363
Peterson <petersonpaganelli@outlook.com>
2022-11-10 00:19:48 -0300

    Excluindo itens não usados

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 // import userEvent from '@testing-library/user-event';
 import App from '../App';
