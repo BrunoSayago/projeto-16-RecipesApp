986f5e505c4ad6a6914c909d77be4ce426abf8a2
Peterson <petersonpaganelli@outlook.com>
2022-11-09 22:30:00 -0300

    Botões DoneRecipes

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -6,7 +6,11 @@ function DoneRecipes() {
   return (
     <div>
       <Header />
-      <h1>Done Recips</h1>
+      <div>
+        <button type="button" data-testid="filter-by-all-btn">All</button>
+        <button type="button" data-testid="filter-by-food-btn">Food</button>
+        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      </div>
     </div>
   );
 }
